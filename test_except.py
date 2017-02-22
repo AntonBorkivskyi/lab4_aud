d83482f45f4235d3cf1b824c20907cc6ea251e82
AntonBorkivskyy <borkivskyy@ucu.edu.ua>
2017-02-22 10:29:05 +0200

    New additions

--- test_except.py
+++ test_except.py
@@ -1,9 +1,18 @@
 def raise_func():
     raise IndexError
+    raise KeyError
 
 def except_func():
     try:
         raise_func()
+    except IndexError:
+        print("Indexxxxx Errrror")
+    except KeyError:
+        print("Keeeey Errrror")
     except Exception:
-        print("Errrror")
-except_func()
+        print("Anoother Errrror")
+
+
+class Myexc(Exception):
+    def myexcept_raise_func(self):
+        except_func()
